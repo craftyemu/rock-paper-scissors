2c081620d20d6c517cd5b2e68bf8487e485e6be6
Chris Smith <146280572+craftyemu@users.noreply.github.com>
2023-12-14 15:23:19 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,41 +1,63 @@
 // Get random choice of options from computer
-let arr = ["Rock", "Paper", "Scissors"];
+let arr = ["ROCK", "PAPER", "SCISSORS"];
 let playerSelection;
-let computerSelection;
+let compSelection;
+let playerWin = 0;
+let compWin = 0;
 
 function getComputerChoice() {
-    let computerSelection = (arr[(Math.floor(Math.random() * arr.length))]);
-    console.log(computerSelection);
-    return computerSelection;
+    let compSelection = (arr[(Math.floor(Math.random() * arr.length))]);
+    console.log(compSelection);
+    return compSelection;
 }
 
 // Get selection from user
 function getPlayerChoice () {
-    let playerSelection = prompt("Which do you choose?: ");
+    let playerSelection = prompt("Rock, Paper, or Scissors?: ");
+    playerSelection = playerSelection.toUpperCase();
     console.log(playerSelection);
     return playerSelection;
 }
 
 // Compare user and computer selections
+// Determine win, lose, or draw
+// Add to score
 function playRound() {
-    let computerSelection = getComputerChoice();
+    let compSelection = getComputerChoice();
     let playerSelection = getPlayerChoice();
 
     if (
-        (playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper") ||
-        (playerSelection === "Paper" && computerSelection === "Rock")) {
-        roundWinner = "Player";
+        (playerSelection === "ROCK" && compSelection === "SCISSORS") ||
+        (playerSelection === "SCISSORS" && compSelection === "PAPER") ||
+        (playerSelection === "PAPER" && compSelection === "ROCK")) {
+        roundWinner = `You win! ${playerSelection} beats ${compSelection}!`;
+        playerWin++;
         }
-    else if (playerSelection === computerSelection) {
-        roundWinner = "Tie";
+    else if (playerSelection === compSelection) {
+        roundWinner = `It's a tie. You both chose ${playerSelection}.`;
         }
-    else (roundWinner = "Computer")    
+    else {
+        roundWinner = `You lose. ${compSelection} beats ${playerSelection}.`;
+        compWin++;
+        }
     
     console.log(roundWinner);
 }
 
-playRound();
+// Repeat rounds
+function playGame() {
+    for (rounds = 0; rounds < 5; rounds++) {
+        playRound();
+    }
+    if (playerWin > compWin) {
+        console.log(`You won the match by a score of ${playerWin} to ${compWin}!`);
+    }
+    else if (compWin > playerWin) {
+        console.log(`You lost the match by a score of ${compWin} to ${playerWin}.`);
+    }
+    else {
+        console.log(`It's a draw. You both scored ${playerWin} points.`);
+    }
+}
 
-// Determine win, lose, or draw
-// Add to score
+playGame();
